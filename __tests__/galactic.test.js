1b6f7c3a7011a17c0480633eccda3caca844bd7b
Erich Richter <erich.richter@gmail.com>
2020-06-26 17:08:29 -0700

    add refactored code clean up code

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -1,74 +1,25 @@
 import { Galactic } from './../src/galactic.js';
 
 describe('Galactic', () => {
+    test('should correctly calculate users years left on planet', () => {
+        const galactic = new Galactic(45, 76, "Earth");
+        galactic.yearsLeftPlanets()
+        console.log(galactic.yearsLeftPlanets())
+        expect(galactic.yearsLeftPlanets()).toEqual(31);
+    });
 
-  test('should correctly create a galactic object', () => {
-    const galactic = new Galactic(44);
-    expect(galactic.age).toEqual(44);
-   });
+    test('should correctly calculate years over life expectancy', () => {
+        const galactic = new Galactic(77, 76, "Venus");
+        galactic.yearsPastExpect()
+        console.log(galactic.yearsPastExpect())
+        expect(galactic.yearsPastExpect()).toEqual(2);
+    });
 
-   test('should correctly calculate users age in days', () => {
-    const galactic = new Galactic(45);
-    galactic.userAgeInDays()
-    console.log(galactic.userAgeInDays())
-    expect(galactic.userAgeInDays()).toEqual(16425);
-   });
-
-   test('should correctly calculate users age Mercury years', () => {
-    const galactic = new Galactic(45);
-    
-    galactic.mercuryAge()
-    console.log(galactic.mercuryAge())
-    expect(galactic.mercuryAge()).toEqual(187);
-   });
-
-   test('should correctly calculate users age Venus years', () => {
-    const galactic = new Galactic(45);
-    galactic.venusAge()
-    console.log(galactic.venusAge())
-    expect(galactic.venusAge()).toEqual(73);
-   });
-
-   test('should correctly calculate users age Mars years', () => {
-    const galactic = new Galactic(45);
-    galactic.marsAge()
-    console.log(galactic.marsAge())
-    expect(galactic.marsAge()).toEqual(24);
-   });
-
-   test('should correctly calculate users age Jupiter years', () => {
-    const galactic = new Galactic(45);
-    galactic.jupiterAge()
-    console.log(galactic.jupiterAge())
-    expect(galactic.jupiterAge()).toEqual(4);
-   });
-
-   test('should correctly calculate users years left on Earth', () => {
-    const galactic = new Galactic(45,76);
-    galactic.yearsLeftEarth()
-    console.log(galactic.yearsLeftEarth())
-    expect(galactic.yearsLeftEarth()).toEqual(31);
-   });
-   
-   test('should correctly calculate users years left on planet', () => {
-    const galactic = new Galactic(45,76,"Earth");
-    galactic.yearsLeftPlanets()
-    console.log(galactic.yearsLeftPlanets())
-    expect(galactic.yearsLeftPlanets()).toEqual(31);
-   });
-  
-   test('should correctly calculate years over life expectancy', () => {
-    const galactic = new Galactic(77,76,"Venus");
-    galactic.yearsPastExpect()
-    console.log(galactic.yearsPastExpect())
-    expect(galactic.yearsPastExpect()).toEqual(2);
-   });
-
-   test('should correctly calculate users years left on inputted planet', () => {
-    const galactic = new Galactic(45,76,"Mars");
-    galactic.planetAge()
-    console.log(galactic.planetAge())
-    expect(galactic.planetAge()).toEqual(5);
-   });
+    test('should correctly calculate users years left on inputted planet', () => {
+        const galactic = new Galactic(45, 76, "Mars");
+        galactic.planetAge()
+        console.log(galactic.planetAge())
+        expect(galactic.planetAge()).toEqual(24);
+    });
 
 });
--- src/galactic.js
+++ src/galactic.js
@@ -6,7 +6,6 @@ export class Galactic {
     }
 
     planetAge() {
-
         if (this.planet === "Mercury") {
             return Math.round(this.age * 365 / 88);
         } else if (this.planet === "Venus") {
@@ -18,44 +17,20 @@ export class Galactic {
         }
     }
 
-
-    userAgeInDays() {
-        return this.age * 365;
-    }
-
-    mercuryAge() {
-        return Math.round(this.age * 365 / 88);
-    }
-
-    venusAge() {
-        return Math.round(this.age * 365 / 225);
-    }
-
-    marsAge() {
-        return Math.round(this.age * 365 / 687);
-    }
-
-    jupiterAge() {
-        return Math.round(this.age * 365 / 4329);
-    }
-
-    yearsLeftEarth() {
-        return Math.round(this.lifeExpectancy - this.age);
-    }
-
-
     yearsLeftPlanets() {
         if (this.planet === "Earth") {
             return Math.round(this.lifeExpectancy - this.age);
         } else if (this.planet === "Mercury") {
             return Math.round(this.lifeExpectancy * 365 / 88) - Math.round(this.age * 365 / 88);
-
-
+        } else if (this.planet === "Venus") {
+            return Math.round(this.lifeExpectancy * 365 / 225) - Math.round(this.age * 365 / 225);
+        } else if (this.planet === "Mars") {
+            return Math.round(this.lifeExpectancy * 365 / 687) - Math.round(this.age * 365 / 687);
+        } else if (this.planet === "Jupiter") {
+            return Math.round(this.lifeExpectancy * 365 / 4329) - Math.round(this.age * 365 / 4329);
         }
-
-
     }
-
+    
     yearsPastExpect() {
         if (this.planet === "Earth" && this.age > this.lifeExpectancy) {
             return this.age - this.lifeExpectancy;
@@ -63,10 +38,10 @@ export class Galactic {
             return Math.round(this.age * 365 / 88) - Math.round(this.lifeExpectancy * 365 / 88)
         } else if (this.planet === "Venus" && this.age > this.lifeExpectancy) {
             return Math.round(this.age * 365 / 225) - Math.round(this.lifeExpectancy * 365 / 225)
+        } else if (this.planet === "Mars" && this.age > this.lifeExpectancy) {
+            return Math.round(this.age * 365 / 687) - Math.round(this.lifeExpectancy * 365 / 687)
+        } else if (this.planet === "Jupiter" && this.age > this.lifeExpectancy) {
+            return Math.round(this.age * 365 / 687) - Math.round(this.lifeExpectancy * 365 / 4329)
         }
-
-
     }
-
 }
-
